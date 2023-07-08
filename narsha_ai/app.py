820225bb8a399cf2bd8946414d58610893862338
otcroz <dbtndus0527@gmail.com>
2023-07-09 03:48:57 +0900

    [feat] Connect AWS Rekognition API - S3에 저장되어 있는 사진 1장을 불러와서 이미지 분석: detect label

--- narsha_ai/app.py
+++ narsha_ai/app.py
@@ -1,9 +1,49 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask import request
+from flask_restx import Resource, Api
+import boto3
+from dotenv import load_dotenv, find_dotenv
+import os
 
+
+
+
+
+
+## flask server ##
 app = Flask(__name__)
+api = Api(app, version='0.0.1')
+
+# env #
+load_dotenv(find_dotenv())
 
 @app.route('/')
 def index():
     return "hello world!"
 
-app.run(host='127.0.0.1', port=8080)
+
+@api.route('/rekognition/detect-label')
+class ObjectDetectionResource(Resource):
+
+    def get(self):
+        filename = request.args['filename'] # set params name: filename
+        client = boto3.client('rekognition', os.environ["REGION"],
+                              aws_access_key_id=os.environ["ACCESS-KEY"],
+                              aws_secret_access_key=os.environ["SECRET-KEY"])
+        response = client.detect_labels(Image={
+            'S3Object': {
+                'Bucket': os.environ["BUCKET-NAME"],
+                'Name': filename
+            }
+        },
+            MaxLabels=5)
+
+        return jsonify({
+                   "result": "success",
+                   "label": response["Labels"]
+               }, 200)
+
+if __name__ == '__main__' :
+    print(os.environ["ACCESS-KEY"])
+    app.run(host='127.0.0.1', port=8000)
+
