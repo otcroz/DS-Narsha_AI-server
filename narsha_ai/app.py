d79b4992b04ddf45879da9916799f84239da4175
[ninano05] <[ohjy0530@duksung.ac.kr]>
2023-07-10 15:00:01 +0900

    [feat] chatGPT API 연결

--- narsha_ai/app.py
+++ narsha_ai/app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask import request, jsonify
+from flask_cors import CORS
+from flask_restx import Resource, Api
+
+import openai
+import os
+
+from narsha_ai.chatGPT import ChatGPT
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def index():
-    return "hello world!"
+api.add_namespace(ChatGPT, '/api')
 
-app.run(host='127.0.0.1', port=8080)
+CORS(app)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True, port=8000)
