f5724afbc3fff7c680aa59773dac5c7af22496a1
Jihye Seo <23goqkfkrl@duksung.ac.kr>
2023-08-03 14:08:25 +0900

    [feat] Friend chatGPT, senior chatGPT 생성 [feat] Friend chatCPT, senior chatGPT 생성

--- narsha_ai/app.py
+++ narsha_ai/app.py
@@ -7,6 +7,10 @@ import os
 ## flask server ##
 from aws import AWS
 from chatGPT import ChatGPT
+from friend_content import FriendContent
+from friend_image import FriendImage
+from senior_content import SeniorContent
+from senior_image import SeniorImage
 
 app = Flask(__name__)
 api = Api(app, version='0.0.1')
@@ -15,6 +19,10 @@ api = Api(app, version='0.0.1')
 load_dotenv(find_dotenv())
 
 api.add_namespace(ChatGPT, '/chat')
+api.add_namespace(FriendContent, '/chat')
+api.add_namespace(FriendImage, '/chat')
+api.add_namespace(SeniorContent, '/chat')
+api.add_namespace(SeniorImage, '/chat')
 api.add_namespace(AWS, '/rekognition')
 
 
