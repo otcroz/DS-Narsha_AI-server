3b1d68774d1bac1d64562e9229eb00a5b220bc93
Jihye Seo <23goqkfkrl@duksung.ac.kr>
2023-08-05 16:36:57 +0900

    [feat] friend_image_content, senior_image_content 추가 및 속성 변경 [feat] friend_image_content, senior_image_content 생성 및 속성 추가

--- narsha_ai/friend_image.py
+++ narsha_ai/friend_image.py
@@ -23,10 +23,10 @@ class CommentMaker(Resource):
             messages=[
                 {"role": "system", "content": "너는 SNS 사용자이다. 조건에 맞게 문자를 댓글처럼 작성하라."},
                 {"role": "system", "content": "문자는 두 문장 이내로 작성한다. 게시글 내용과 관련있는 내용으로 작성한다."},
-                {"role": "system", "content": "주어진 단어들을 연결하여 하나의 이야기에 대한 내용으로 작성한다."},
+                {"role": "system", "content": "너는 9살이다. 9살 동갑 친구에게 말하는 것처럼 답한다."},
+                {"role": "system", "content": "너는 반말을 해야한다."},
+                # {"role": "system", "content": "주어진 단어들을 연결하여 하나의 이야기에 대한 내용으로 작성한다."},
                 {"role": "system", "content": "의문을 품지 않는다."},
-                {"role": "system", "content": "너는 9살이다."},
-                {"role": "system", "content": "밝고 친절하고, 긍정적으로 말한다.친구한테 말하듯 반말을 한다."},
                 {"role": "system", "content": "상황은 작성하지 않는다."},
                 {"role": "system", "content": "댓글임을 작성하지 않는다."},
                 {"role": "user", "content": f"1. 게시글 내용: ${post_content}"},
