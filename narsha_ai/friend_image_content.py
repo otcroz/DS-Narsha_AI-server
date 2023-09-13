9e093b4e27ad76bc325e30507e63e8036ea9d5e5
Jihye Seo <23goqkfkrl@duksung.ac.kr>
2023-09-13 19:53:43 +0900

    [feat] friend chatGPT 수정 [feat] friend chatGPT 수정

--- narsha_ai/friend_image_content.py
+++ narsha_ai/friend_image_content.py
@@ -26,14 +26,18 @@ class CommentMaker(Resource):
                 {"role": "system", "content": "문자는 두 문장 이내로 작성한다. 게시글 내용과 관련있는 내용으로 작성한다."},
                 {"role": "system", "content": "너는 9살이다. 9살 동갑 친구에게 말하는 것처럼 답한다."},
                 {"role": "system", "content": "너는 반말을 한다."},
-                # {"role": "system", "content": "밝고 친절하고, 긍정적으로 말한다."},
-                # {"role": "system", "content": "주어진 단어들을 연결하여 하나의 이야기에 대한 내용으로 작성한다."},
-                {"role": "system", "content": "의문을 품지 않는다."},
                 {"role": "system", "content": "상황은 작성하지 않는다."},
                 {"role": "system", "content": "댓글임을 작성하지 않는다."},
+                {"role": "system", "content": "존댓말을 해서는 안된다. 반말로 문장을 만들어라."},
+                {"role": "system", "content": "줄바꿈 문자를 절대 사용하지 않는다. 큰 따옴표를 적지 않는다. 숫자로 문장을 시작하지 않는다."},
+                {"role": "system", "content": "입력문자에 영어가 있어도 절대로 영어를 사용하지 않는다. 반드시 한국어만을 사용한다."},
+                {"role": "system", "content": "문자는 두 문장 이내로 작성한다. 게시글 내용과 게시글 사진에 관련있는 내용으로 작성한다. 하지만 사진과 관련한 모든 키워드를 언급할 필요는 없다."},
+                {"role": "system", "content": "You have to write down all the sentences in one line. Don't change the line"},
+                {"role": "system", "content": "act as a friend"},
+                {"role": "system", "content": "write in informal style"},
                 {"role": "user", "content": f"1. 게시글 내용: ${post_content} 2. 게시글 사진: ${post_image}"},
             ],
-            temperature=0.8,
+            temperature=0.6,
             max_tokens=2048
         )
 
