f9aee5f1b07f86c65c4e78f9498e734bbf955ee1
otcroz <dbtndus0527@gmail.com>
2023-09-15 03:59:54 +0900

    [feat] Implement to check personal information logic - Implement to check personal information logic

--- narsha_ai/text_labeling/replace_word.py
+++ narsha_ai/text_labeling/replace_word.py
@@ -1,3 +1,5 @@
+import re
+
 replace_words_dic = {
     "미친": "대박", "존나": "진짜", "놈": "사람", "년": "사람", "련": "사람", "급식충": "학생", "새끼": "사람",
     "새키": "사람", "애미": "엄마", "애비": "아빠", "한남": "한국 남자", "한녀": "한국 여자", "충": "집단", "계집": "여자",
@@ -21,3 +23,27 @@ def replace(input):
                 break
 
     return res
+
+
+def detect_personal_info(input):
+    personal_info = []
+
+    # pattern
+    national_id_pattern = r'\d{6}-\d{7}'
+    passport_pattern = r'[A-Z]\d{3}[A-Z]\d{4}'
+    driver_pattern = r'\d{2}-\d{2}-\d{6}-\d{2}'
+    credit_pattern = r'\d{4}[ ,-]\d{4}[ ,-]\d{4}[ ,-]\d{4}'
+    phone_pattern = r'010[ ,-]\d{4}[ ,-]\d{4}'
+    email_pattern = r'([\w!-_\.]+)@([\w!-_\.]+)\.[\w]{2,3}'
+
+    # check
+    personal_info.append(re.findall(national_id_pattern, input))
+    personal_info.append(re.findall(passport_pattern, input))
+    personal_info.append(re.findall(driver_pattern, input))
+    personal_info.append(re.findall(credit_pattern, input))
+    personal_info.append(re.findall(phone_pattern, input))
+    personal_info.append(re.findall(email_pattern, input))
+
+    print(personal_info)
+
+    return personal_info
