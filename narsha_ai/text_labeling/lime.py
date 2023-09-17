0a0b1dd32e90f470a65033504b027d1d493b2e6e
otcroz <dbtndus0527@gmail.com>
2023-09-18 08:29:09 +0900

    [fix] fix to return form in lime - fix to return form in lime

--- narsha_ai/text_labeling/lime.py
+++ narsha_ai/text_labeling/lime.py
@@ -18,7 +18,8 @@ from text_labeling import preprocess
 max_len = 64
 batch_size = 64
 text_count = 0
-res_arr = {"input": [], "result": []}
+res_arr = {"input": [], "result": {}}
+
 
 TextFiltering = Namespace('TextFiltering')
 
@@ -29,42 +30,36 @@ class LimeTextFiltering(Resource):
         global text_count
         global res_arr
 
+        curse_res_arr = {}
+        personal_res_arr = []
+        total_curse_arr = []
+
         # preprocessing input
         input = request.get_json()
         split_sentence = input['input'].split('. ')  # split sentence
-        # print("split_sentence: ", split_sentence)
 
         preprocess_result = preprocess.preprocess_text(split_sentence)
 
         # delete blank item
-        personal_sentence = []
-        for item in split_sentence:
+        personal_sentence = ""
+        for idx, item in enumerate(split_sentence):
             if len(item) == 0:
                 continue
-            personal_sentence.append(item)
-        # print(personal_sentence)
-
-        # clear result object
-        res_arr['input'].clear()
-        res_arr['result'].clear()
-
-        for i in range(0, len(split_sentence)):
-            if i == len(split_sentence)-1:
-                res_arr['input'].append(split_sentence[i])
+            if idx == len(split_sentence)-1:
+                personal_sentence += item
             else:
-                res_arr['input'].append(split_sentence[i] + ".")
+                personal_sentence += item + ". "
+        res_arr["input"] = personal_sentence
 
-        # start repeat(for)
+        # define object
+        res_object = {
+            "curse": [],
+            "personal": [],
+        }
+
         for idx, res in enumerate(preprocess_result):
-
-            res_object = {
-                "curse": [],
-                "personal": [],
-            }
-
             # 1. check sentence through koBERT
             classify_res = kobert_text.kobert_classify(res)
-            # print(classify_res)
 
             if classify_res != 0:
                 # split to word unit
@@ -75,8 +70,7 @@ class LimeTextFiltering(Resource):
 
                 if len(res_temp) == 1:  # if word count is 1, except LIME
                     curse_res = replace_word.replace(res_temp[0])
-                    res_object["curse"].append(curse_res)
-                    # print(res_object)
+                    curse_res_arr[res_temp] = curse_res
                 else:
                     # lime: check curse sentence
                     exp = lime_exp(res)
@@ -84,31 +78,41 @@ class LimeTextFiltering(Resource):
 
                     # check to exist curse_arr or not
                     if len(curse_arr) == 0:
-                        res_object["curse"].append({"문장": True})  # input sentence
+                        total_curse_arr.append(personal_sentence[idx])
                     else:
                         # curse replace
                         for curse in curse_arr:
                             curse_res = replace_word.replace(curse)
-                            res_object["curse"].append(curse_res)
-
-            else:
-                res_object["curse"].append(None)
+                            curse_res_arr[curse] = curse_res
 
             # 2. check personal info
-            personal_res = replace_word.detect_personal_info(personal_sentence[idx])
+            personal_res = replace_word.detect_personal_info(split_sentence[idx])
 
             if len(personal_res) != 0:
                 for item in personal_res:
-                    res_object["personal"].append(item)
-            else:
-                res_object["personal"].append(None)
+                    personal_res_arr.append(item)
 
-            # 3. check curse and personal
-            # print(res_object)
-            if res_object["curse"][0] is None and res_object["personal"][0] is None:
-                res_arr["result"].append(True)
-            else:
-                res_arr["result"].append(res_object)
+        # 4. check curse and personal
+        if len(total_curse_arr) != 0:  # check curse
+            res_object["total"] = total_curse_arr
+        else:
+            res_object["total"] = [None]
+
+        if len(personal_res_arr) != 0: # check personal
+            res_object["personal"] = personal_res_arr
+        else:
+            res_object["personal"] = [None]
+
+        if len(curse_res_arr) != 0:  # check total curse sentence
+            res_object["curse"] = curse_res_arr
+        else:
+            res_object["curse"] = None
+
+        # total
+        if len(curse_res_arr) == 0 and len(personal_res_arr) == 0 and len(total_curse_arr) == 0:
+            res_arr["result"] = True
+        else:
+            res_arr["result"] = res_object
 
         return res_arr
 
